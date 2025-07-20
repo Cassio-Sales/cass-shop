f1efda59f1a0b4d8ccac2068b44c61d03e8fe7b7
Cassio-Sales <cassiososales@gmail.com>
2025-07-20 16:13:02 +0100

    feat: checkout sucess, product details and redirect to homepage

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,22 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { stripe } from "../../lib/stripe"
+import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe'
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse){ 
-  const { priceId } = req.body;
-  
-   if (req.method != 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' })
-   }
-   
-   
-   if (!priceId) { 
-    return res.status(400).json({ error: 'Price not found.'})
-   }
-  
-    const successUrl = `${process.env.NEXT_URL}/success`
-  const cancelUrl =  `${process.env.NEXT_URL}/` 
-  
-  const checkoutSection = await stripe.checkout.sessions.create({
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { priceId } = req.body
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed.' })
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found.' })
+  }
+
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+
+  const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
     cancel_url: cancelUrl,
     mode: 'payment',
@@ -27,7 +29,8 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse){
       }
     ]
   })
+
   return res.status(201).json({
-    checkoutUrl: checkoutSection.url,
+    checkoutUrl: checkoutSession.url
   })
 }
